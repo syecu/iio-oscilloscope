2a1f095799fbfa5708b8dc0094ad73c400d703cc
Robin Getz <Robin.Getz@analog.com>
2015-04-30 17:59:12 -0400

    oscplot: support enabling various marker types via "marker_type" in ini files
    For example, adding the following line in an ini file under a capture
    window section will enable peak markers:
    
    marker_type = Peak Markers

--- oscplot.c
+++ oscplot.c
@@ -4646,9 +4646,27 @@ int osc_plot_ini_read_handler (OscPlot *plot, int line, const char *section,
 					gtk_window_move(GTK_WINDOW(priv->window), priv->plot_x_pos, priv->plot_y_pos);
 				}
 			} else if (MATCH_NAME("marker_type")) {
-				set_marker_labels(plot, (gchar *)value, MARKER_NULL);
-				for (i = 0; i <= MAX_MARKERS; i++)
-					priv->markers[i].active = FALSE;
+				if (!strncmp(value, PEAK_MRK, strlen(PEAK_MRK))) {
+					printf("set to peak\n");
+					priv->marker_type = MARKER_PEAK;
+				} else if (!strncmp(value, FIX_MRK, strlen(FIX_MRK))) {
+					printf("set to fixed\n");
+					priv->marker_type = MARKER_FIXED;
+				} else if (!strncmp(value, SINGLE_MRK, strlen(SINGLE_MRK))) {
+					printf("set to single tone markers\n");
+					priv->marker_type = MARKER_ONE_TONE;
+				} else if (!strncmp(value, DUAL_MRK, strlen(DUAL_MRK))) {
+					printf("set to two tone markers\n");
+					priv->marker_type = MARKER_TWO_TONE;
+				} else if (!strncmp(value, IMAGE_MRK, strlen(IMAGE_MRK))) {
+					printf("set to image markers\n");
+					priv->marker_type = MARKER_IMAGE;
+				} else {
+					printf("setting all off\n");
+					set_marker_labels(plot, (gchar *)value, MARKER_NULL);
+					for (i = 0; i <= MAX_MARKERS; i++)
+						priv->markers[i].active = FALSE;
+				}
 			} else if (MATCH_NAME("save_png")) {
 				save_as(plot, value, SAVE_PNG);
 			} else if (MATCH_NAME("cycle")) {
@@ -4661,21 +4679,6 @@ int osc_plot_ini_read_handler (OscPlot *plot, int line, const char *section,
 				fd = fopen(value, "a");
 				if (!fd)
 					return 0;
-
-				for (i = 0; i < num_devices; i++) {
-					struct iio_device *dev = iio_context_get_device(ctx, i);
-					const char *id = iio_device_get_name(dev) ?:
-						iio_device_get_id(dev);
-					if (!strcmp(id, "cf-ad9643-core-lpc") || !strcmp(id, "cf-ad9361-lpc")) {
-						for (i = 0; i < iio_device_get_channels_count(dev); i++) {
-							struct iio_channel *chn = iio_device_get_channel(dev, i);
-							struct extra_info *ch_info = iio_channel_get_data(chn);
-							const char *name = iio_channel_get_name(chn) ?:
-										iio_channel_get_id(chn);
-							fprintf(fd, "%s:%lf", name, ch_info->lo_freq);
-						}
-					}
-				}
 
 				for (i = 0; i <= MAX_MARKERS; i++) {
 					if (priv->markers[i].active) {
